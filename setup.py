625adadcb7baa6cd322e89e6b3d1f3bb1ece6be1
Pavan Managoli <your_github_email@example.com>
2026-06-26 23:12:02 +0200

    setup file added

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='holiday_management',
+    version='0.1.0',
+    author="Pavankumar Umesh Managoli",
+    author_email="pumanagoli@gmail.com",
+    packages=find_packages()
+)
